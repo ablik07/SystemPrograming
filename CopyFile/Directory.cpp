8d1c8c39017fef1ddfbbe64ba4d5165cf0ce7cd6
Ruslan <fedotovoruslan617@gmail.com>
2026-05-23 21:23:25 +0300

    2. создание подкатолога

--- CopyFile/Directory.cpp
+++ CopyFile/Directory.cpp
@@ -18,3 +18,26 @@ int main()
     cout << "The directory is created." << endl;
     return 0;
 }
+
+// ѕример создани€ подкаталога
+
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // создаем подкаталог
+    if (!CreateDirectoryEx("C:\\Users\\karolina\\Downloads\\demo", "C:\\Users\\karolina\\Downloads\\demo_1", NULL))
+    {
+        cerr << "Create directory failed." << endl
+            << "The last error code: " << GetLastError() << endl;
+        cout << "Press any key to finish.";
+        cin.get();
+        return 0;
+    }
+
+    cout << "The subdirectory is created." << endl;
+    return 0;
+}
