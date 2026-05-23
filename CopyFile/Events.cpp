6ab24241b051e9fc2cd30ff98fad06dc99f495b2
Ruslan <fedotovoruslan617@gmail.com>
2026-05-23 21:27:29 +0300

    переименование файла

--- CopyFile/Events.cpp
+++ CopyFile/Events.cpp
@@ -1,3 +1,24 @@
+//#include <windows.h>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    if (!DeleteFile("C:\\Users\\karolina\\Downloads\\demo\\test.txt"))
+//    {
+//        cerr << "Delete file failed." << endl
+//            << "The last error code: " << GetLastError() << endl;
+//        cout << "Press any key to finish.";
+//        cin.get();
+//        return 0;
+//    }
+//
+//    cout << "File deleted successfully." << endl;
+//    return 0;
+//}
+//
+
 #include <windows.h>
 #include <iostream>
 
@@ -5,15 +26,17 @@ using namespace std;
 
 int main()
 {
-    if (!DeleteFile("C:\\Users\\karolina\\Downloads\\demo\\test.txt"))
+    // Переименование (перемещение) файла
+    if (!MoveFile("C:\\Users\\karolina\\Downloads\\demo\\old.txt",
+        "C:\\Users\\karolina\\Downloads\\demo\\new.txt"))
     {
-        cerr << "Delete file failed." << endl
+        cerr << "Move file failed." << endl
             << "The last error code: " << GetLastError() << endl;
         cout << "Press any key to finish.";
         cin.get();
         return 0;
     }
 
-    cout << "File deleted successfully." << endl;
+    cout << "File moved/renamed successfully." << endl;
     return 0;
 }
