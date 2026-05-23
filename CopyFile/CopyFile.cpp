e5d4b40bb1c65e2f36664a48c68a59cfb98e010b
Ruslan <fedotovoruslan617@gmail.com>
2026-05-23 21:45:04 +0300

    Дочерний процесс

--- CopyFile/CopyFile.cpp
+++ CopyFile/CopyFile.cpp
@@ -58,7 +58,7 @@ int main(int argc, char* argv[])
             {
                 processedLine[i + 1] = ' ';
                 totalReplacements++;
-                i++; // пропускаем следующий символ
+                i++;
             }
         }
 
