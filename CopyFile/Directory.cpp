3ca78e398945d2bb55af1edb3ad8eca29ee15511
Ruslan <fedotovoruslan617@gmail.com>
2026-05-23 21:24:13 +0300

    3. поиск файлов

--- CopyFile/Directory.cpp
+++ CopyFile/Directory.cpp
@@ -41,3 +41,39 @@ int main()
     cout << "The subdirectory is created." << endl;
     return 0;
 }
+
+// Пример поиска файлов в каталоге
+
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    HANDLE hFindFile;
+    WIN32_FIND_DATA fd;
+
+    // находим первый файл
+    hFindFile = FindFirstFile("C:\\Users\\karolina\\Downloads\\demo_\\*", &fd);
+    if (hFindFile == INVALID_HANDLE_VALUE)
+    {
+        cerr << "Find first file failed." << endl
+            << "The last error code: " << GetLastError() << endl;
+        cout << "Press any key to finish.";
+        cin.get();
+        return 0;
+    }
+
+    // выводим на консоль имя первого файла
+    cout << "The first file name: " << fd.cFileName << endl;
+
+    // находим следующий файл и выводим на консоль его имя
+    while (FindNextFile(hFindFile, &fd))
+    {
+        cout << "The next file name: " << fd.cFileName << endl;
+        // закрываем дескриптор поиска
+        FindClose(hFindFile);
+        return 0;
+    }
+}
